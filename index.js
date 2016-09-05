5c249cfbcd2afd01ab457003d0288ce7f4990a2f
Rene Weber <weber.rene@live.de>
2016-09-05 23:47:15 +0100

    Fix map call

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const resolver = require('./lib/resolver')
 const async = require('./lib/async')
 
 module.exports.configure = function(config, cb) {
-  const resolveFuncs = Object.keys(config).map(c => {
+  const resolveFuncs = Object.keys(config).map(c =>
     (cb) => resolver.resolve(config[c], cb)
-  })
+  )
 
   async.merge(resolveFuncs, results => {
     const errors = result.filter(r => r.err !== undefined)
