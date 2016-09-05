dce2966eec4d580db6afd1c26a8d0df873af9c4d
Rene Weber <weber.rene@live.de>
2016-09-05 20:29:52 +0100

    Add test for env provider

---
+++ test/provider/env.spec.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const env = require('../../lib/provider/env')
+
+const keyToCheck = "testKey"
+const varToCheck = "testVar"
+
+process.env[keyToCheck] = varToCheck
+
+env.init({}, (err, envProvider) => {
+  envProvider.read((err, res) => {
+    assert.equal(res[keyToCheck], varToCheck, "Env variables not found / not matching.")
+  })
+})
