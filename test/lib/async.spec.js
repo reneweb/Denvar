59c31bbec3c3c566a828c763fd5c7aa80707a951
Rene Weber <weber.rene@live.de>
2016-09-06 00:37:35 +0100

    Add test for async

---
+++ test/lib/async.spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const async = require('../../lib/async')
+
+const functions = [(cb) => cb(null, 1), (cb) => cb(null, 2), (cb) => cb(null, 3)]
+
+async.merge(functions, results => {
+  assert.equal(results[0].res, 1)
+  assert.equal(results[1].res, 2)
+  assert.equal(results[2].res, 3)
+})
