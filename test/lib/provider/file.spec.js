91e86cf48fd3646ca2c1b2e9aa6db84550c77509
Rene Weber <weber.rene@live.de>
2016-09-08 00:48:26 +0100

    Provide conversion to bool/number for property formatted files

--- test/lib/provider/file.spec.js
+++ test/lib/provider/file.spec.js
@@ -5,6 +5,14 @@ const file = require('../../../lib/provider/file')
 const keyToCheck = 'testKey'
 const varToCheck = 'testVar'
 
+const numberKeyToCheck = 'numberTestKey'
+const numberVarToCheck = 123
+
+const boolKeyToCheck = 'boolTestKey'
+const boolVarToCheck = false
+
+const content = `${keyToCheck}=${varToCheck}\n${numberKeyToCheck}=${numberVarToCheck}\n${boolKeyToCheck}=${boolVarToCheck}`
+
 const cleanUp = f => {
   try {
     f()
@@ -13,10 +21,12 @@ const cleanUp = f => {
   }
 }
 
-fs.writeFileSync('./testFile', `${keyToCheck}=${varToCheck}`)
+fs.writeFileSync('./testFile', content)
 
 file.init({path: './testFile', format: 'property'}, (err, provider) => {
-  provider.read((err, res) => cleanUp(() =>
+  provider.read((err, res) => cleanUp(() => {
     assert.equal(res[keyToCheck], varToCheck, 'Provided variables not found / not matching.')
-  ))
+    assert.equal(res[numberKeyToCheck], numberVarToCheck, 'Provided variables not found / not matching.')
+    assert.equal(res[boolKeyToCheck], boolVarToCheck, 'Provided variables not found / not matching.')
+  }))
 })
