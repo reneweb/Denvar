5665ec3b2f43df625c08f340fc16f4e29b311c6e
Rene Weber <weber.rene@live.de>
2016-09-30 23:52:10 +0100

    Add consul extension

---
+++ dvar-ext-consul/index.js
@@ -0,0 +1,31 @@
+const consulClient = require('consul')
+
+module.exports.init = (config, cb) => {
+  const host = config.host
+  const port = config.port
+  const secure = config.secure
+  const keyPrefix = config.keyPrefix
+
+  const consul = consulClient({
+    host: host,
+    port: port,
+    secure: secure,
+    keyPrefix: keyPrefix
+  })
+
+  const readF = cb => {
+    consul.kv.get({
+      key: keyPrefix,
+      recurse: true
+    }, (err, data, res) => {
+      const transformedData = data.reduce((prev, curr) => {
+        prev[curr.Key] = curr.Value
+        return prev
+      }, {})
+
+      cb(err, transformedData)
+    })
+  }
+
+  cb(null, {read: readF})
+}
---
+++ dvar-ext-consul/test/index.spec.js
@@ -0,0 +1,44 @@
+const expect = require('chai').expect
+const dvarConsul = require('../index')
+const http = require('http')
+
+describe('dvar-ext-consul', () => {
+  const keyToCheck = 'testKey'
+  const varToCheck = 'testValue'
+
+  const contentJson = [{
+    CreateIndex: 6,
+    ModifyIndex: 6,
+    LockIndex: 0,
+    Key: keyToCheck,
+    Flags: 0,
+    Value: new Buffer(varToCheck).toString('base64')
+  }]
+
+  before(() => {
+    http.createServer(function(request, response) {
+      var url = request.url
+      if (url.startsWith('/v1/kv/test')) {
+        response.writeHead(200, {'Content-Type': 'application/json'})
+        response.end(JSON.stringify(contentJson))
+      } else {
+        response.status = 500
+        response.end()
+      }
+    }).listen(9111)
+  })
+
+  it('should read config from consul', done => {
+    dvarConsul.init({
+      host: 'localhost',
+      port: 9111,
+      secure: false,
+      keyPrefix: 'test'
+    }, (err, provider) => {
+      provider.read((err, res) => {
+        expect(res[keyToCheck]).to.equal(varToCheck)
+        done()
+      })
+    })
+  })
+})
